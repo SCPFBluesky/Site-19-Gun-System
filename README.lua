57805aac6b6a90a8bf6fbb5c61092b340a87f061
SCPFBluesky <144206470+SCPFBluesky@users.noreply.github.com>
2024-09-16 17:31:28 -0500

    Update README.lua

--- README.lua
+++ README.lua
@@ -2,7 +2,7 @@
 This gun system was made to replicate the gun system found in "Site-19 Roleplay" made by AdministratorGnar and ThunderGemios10
 This system includes almost all features of the gun system including Mobile support.]]
 --[Inside the Settings inside "FireCore" (ServerScript) there are these 8 Options you can customise to your liking]
-  ShowBlood = false, -- Setting this to true will enable blood, setting it to false will disable blood
+ 	ShowBlood = false, -- Setting this to true will enable blood, setting it to false will disable blood
 	ShowMuzzleEffects = true, --Setting this to true will enable MuzzleEffects, setting it to false will disable MuzzleEffects
 	ShowV1MuzzleEffects = false, -- Setting this to true instend of showing V3+ Muzzle effects it will show alpha muzzleeffects (V1/V2)
 	ShellEjection = false, -- Setting this to true will enable ShellEjection (A removed feature from the alpha verisions of Site-19 Roleplay)
