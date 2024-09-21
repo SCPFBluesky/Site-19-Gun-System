e034f07c79ae38f8a0e1fba3d9753817597afb33
SCPFBluesky <144206470+SCPFBluesky@users.noreply.github.com>
2024-09-21 02:40:51 -0500

    Create TeamPriorityModule.lua

---
+++ TeamPriorityModule.lua
@@ -0,0 +1,5 @@
+return {
+	["Priority1"] = {"Class D", "Chaos Insurgency"}, -- Your class d team and chaos insurgency teams here
+	["Priority2"] = {"FoundationTeam1", "FoundationTeam2"}, -- Normal foundation teams here
+	["Priority3"] = {"TKFoundationTeam1", "TKFoundationTeam2"} -- TK foundation teams here
+}
